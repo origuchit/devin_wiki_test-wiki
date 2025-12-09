39bf0657ed276ef0f989f0bcf0a322602140e6d9
Test User <test@example.com>
2025-12-09 11:33:10 +0900

    Update main application to use logger and utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // メインアプリケーションファイル
+const Logger = require('./logger');
+const { formatDate } = require('./utils');
+
+const logger = new Logger();
+
 console.log('Hello, World!');
+console.log('Current date:', formatDate(new Date()));
 
 const config = require('./config');
 console.log('Config loaded:', config);
-
